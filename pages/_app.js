ee13822b0fac6d233b18fa9fe14cd8f4c9bb60e1
Thomas Kirst <thomas.kirst@gmail.com>
2023-09-13 11:38:20 +0200

    First steps towards the solution, lifting the state up

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }) {
+  const initialLights = [
+    { id: 1, name: "Living name", isOn: false },
+    { id: 2, name: "Kitchen", isOn: false },
+    { id: 3, name: "Bedroom", isOn: false },
+    { id: 4, name: "Bathroom", isOn: false },
+    { id: 5, name: "Garage", isOn: false },
+    { id: 6, name: "Porch", isOn: false },
+    { id: 7, name: "Garden", isOn: false },
+    { id: 8, name: "Office", isOn: false },
+  ];
+
+  const [lights, setLights] = useState(initialLights);
+
+  function handleToggle(id) {
+    setLights(
+      lights.map((light) =>
+        light.id === id ? { ...light, isOn: !light.isOn } : light
+      )
+    );
+  }
+
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} lights={lights} onToggle={handleToggle} />
     </Layout>
   );
 }
